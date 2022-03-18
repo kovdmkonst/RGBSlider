4aef11602c0c3526df2eeabe69158f72ec71e6d8
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2022-03-18 20:59:49 +1000

    add new view controller

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    var redColorValue: CGFloat = 0
+    var greenColorValue: CGFloat = 0
+    var blueColorValue: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 10
+        
+        changeRedValue()
+        changeBlueValue()
+        changeGreenValue()
+        
     }
+    
+    @IBAction func changeRedValue() {
+        let redValue = redSlider.value * 255
+        redTextField.text = String(Int(redValue))
+        changeViewColor()
 
+    }
+    
+    @IBAction func changeGreenValue() {
+        let greenValue = greenSlider.value * 255
+        greenTextField.text = String(Int(greenValue))
+        changeViewColor()
 
+    }
+    
+    @IBAction func changeBlueValue() {
+        let blueValue = blueSlider.value * 255
+        blueTextField.text = String(Int(blueValue))
+        changeViewColor()
+    }
+    
+    
+    private func changeViewColor() {
+        redColorValue = CGFloat(redSlider.value)
+        greenColorValue = CGFloat(greenSlider.value)
+        blueColorValue = CGFloat(blueSlider.value)
+        
+        colorView.layer.backgroundColor = CGColor(
+            red: redColorValue,
+            green: greenColorValue,
+            blue: blueColorValue,
+            alpha: 1.0)
+    }
+    
 }
 
